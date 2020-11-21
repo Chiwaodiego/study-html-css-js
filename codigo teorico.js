8001d1b94d184ac0303cd51ccabae3674d31dc0e
Chiwaodiego <73324237+Chiwaodiego@users.noreply.github.com>
2020-11-21 08:50:45 -0300

    ac

---
+++ codigo teorico.js
@@ -0,0 +1,44 @@
+*/alert("hola")*/
+4)variables
+tipos de datos
+string= "cadena texto"
+number= 21
+booleano= true or false
+symbol=
+img=
+; se para variables
+, cadena de variables
+las variables se puede escribir junto ejem: let n1 = 2, n2 = 4; para ahorrar codigo 
+var number = 15;
+let number = "se usa numero porque nos limite el alcanse del bloque";
+const = "es una variable que nunca cambia su valor";
+scope es una condicion o ambito que es valida por dentro de la variable a fuera no;
+hoisting ;
+
+casos especiales de datos:
+undefined
+null se aclara que es una var vacia pero es nula, let n1 = null;
+nan "no se puede operar con letras"
+
+prompt es un funcion para almacenar datos y luegos se puede pedir
+
+5)Operadores en javascript
+-Operadores de asignacion (IMG)
+	asigna un valor al operando de la izquierda basadoe n el valor del operando de la derecha
+	ej 
+	let n= 10;
+	n +=5
+	ej 
+	%= te muesta 0 o el numero entero del resto
+	document.write(n)
+-Operadores Aritméticos(IMG)
+	toman valores numericos(ya sean literales o variables) como sus operandos y retornan un valor numerico unico.
+	num1 = 10;
+	num2 = 5;
+	num1 --;
+	result = num1;
+	alert(result)
+	ej exponecial result = num1**2;
+	negativo 	result = -num1;
+
+
