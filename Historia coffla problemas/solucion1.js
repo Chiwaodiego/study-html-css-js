d8e1304375d7df3766670b6891117e8b78ded9fe
Chiwaodiego <73324237+Chiwaodiego@users.noreply.github.com>
2020-11-22 18:08:54 -0300

    ac

---
+++ Historia coffla problemas/solucion1.js
@@ -0,0 +1,16 @@
+
+din1 = prompt("Cuanto dinero Roberto?");
+din2 = prompt("Cuanto dinero Pedro?");
+din3 = prompt("Cuanto dinero Cofla?");
+
+if (din1 >= 0.6 && din1 < 1) { alert ("comprate helado de agua") }
+	else if (din1 >= 1.0 && din1 <1.6){ alert ("comprate helado de crema") }
+	else if (din1 >= 1.7 && din1 <1.8){ alert ("comprate Bombón helado marca heladovich") }
+	else if (din1 >= 1.8 && din1 <2.9){ alert ("comprate Bombón helado marca helardo") }
+	else if (din1 >= 2.9 ){ alert ("comprate Potecito de helado on confites o el Pote de 1/4 KG ") }
+	else { "Lo siento, no le alcanza"}
+
+
+
+
+
