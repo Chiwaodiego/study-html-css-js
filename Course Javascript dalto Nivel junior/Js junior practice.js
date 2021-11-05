2537deb08dc38140bd6dce40f2eafe83328afcae
Chiwaodiego <73324237+Chiwaodiego@users.noreply.github.com>
2021-11-04 21:11:37 -0300

    Gestion de archivos

---
+++ Course Javascript dalto Nivel junior/Js junior practice.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Borrador</title>
+	<link rel="stylesheet" type="text/css" href="css2.css">
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+	<div class="container">
+HOLA SOY DIEGO
+
+
+	</div>
+
+
+
+
+
+</body>
+</html>
+		
