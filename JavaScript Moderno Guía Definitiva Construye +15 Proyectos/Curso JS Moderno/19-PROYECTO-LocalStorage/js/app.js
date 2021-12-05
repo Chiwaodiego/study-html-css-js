d135c03b645dc0e2d64a428019a45f5b9bb3531a
Chiwaodiego <73324237+Chiwaodiego@users.noreply.github.com>
2021-12-04 22:35:08 -0300

    proyecto finalizado

--- JavaScript Moderno Guía Definitiva Construye +15 Proyectos/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ JavaScript Moderno Guía Definitiva Construye +15 Proyectos/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -7,7 +7,18 @@ let tweets = [];
 eventListeners();
 
 function eventListeners() {
+    // cuando el usuario agrega un nuevo tweet
     formulario.addEventListener('submit', agregarTweet);
+
+    // cuando el documento esta listo, aplicar DOMcontentLoaded
+    document.addEventListener('DOMContentLoaded',() => {
+        tweets = JSON.parse( localStorage.getItem('tweets')) || [];
+        // agregar un or [] con un array vacio para que no salte errors null en la consola
+
+        console.log(tweets);
+
+        crearHTML();
+    });
 }
 
 
@@ -32,9 +43,11 @@ function agregarTweet (e) {
     //Añadir al arreglo de tweets;
     tweets = [...tweets, tweetObj];
 
-    // Una ves agregado vamos a crer el HTML
+    // Una ves agregado vamos a crear el HTML
     crearHTML();
     
+    // reiniciar el formulario , metodo reset
+    formulario.reset();
 }
 
 
@@ -61,20 +74,65 @@ s
 // Muestra un listado de los tweets
 
 function crearHTML() {
+
+    limpiarHTML();
+
     if (tweets.length > 0) {
         tweets.forEach(tweet => {
+
+            // agregar un boton de eliminar
+            const btnEliminar = document.createElement('a');
+            btnEliminar.classList.add('borrar-tweet');
+            btnEliminar.innerText = 'X';
+
+            // añadir la funcion de eliminar del DOM
+            btnEliminar.onclick = () =>{
+                // pasar los parametros para indentificar que elemento borrar
+                borrarTweet(tweet.id);
+            }
+
             //crear el HTML
 
             const li = document.createElement('li');
 
-            //añadir  el texto
+            // añadir el texto
+                // retorna y añade  el texto, recuerda que tweet 1st es la variable y 2rd tweet es el valor agregado
             li.innerText = tweet.tweet;
+            
+            // Asignar el boton
 
-            // insertarlo en el html
+            li.appendChild(btnEliminar);
+
+       
+
+            // insertarlo en el html del id
             listaTweets.appendChild(li);
 
 
         });
         
+    }
+    sincronizarStorage();
+}
+
+// guardar informacion en local storage del array almacenado.
+function sincronizarStorage () {
+   
+    localStorage.setItem('tweets',JSON.stringify(tweets));
+    
+}
+
+// eliminar un tweet con filter, con parametro del id data now
+function borrarTweet(id) {
+    tweets = tweets.filter( tweet => tweet.id !== id);
+
+    crearHTML();
+}
+
+
+//Limpiar el html 
+function limpiarHTML(){
+    while ( listaTweets.firstChild){
+        listaTweets.removeChild(listaTweets.firstChild);
     }
 }
